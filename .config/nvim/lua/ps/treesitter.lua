ba9ecbeebb2119326d54a1cc2a080efb05ac1b8c
Pranjal Soni <soni.pranjals17@gmail.com>
2023-08-12 16:03:16 +0530

    nvim: use autopairs for autotag and ts-autotag for tag renaming
    Also, `function` endwise for lua has been expanded.

--- .config/nvim/lua/ps/treesitter.lua
+++ .config/nvim/lua/ps/treesitter.lua
@@ -5,6 +5,9 @@ require'nvim-treesitter.configs'.setup {
   },
   autotag = {
     enable = true,
+    enable_rename = true,
+    enable_close = false,
+    enable_close_on_slash = false,
   },
   additional_vim_regex_highlighting = false,
 }
