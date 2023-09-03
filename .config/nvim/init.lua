1f56babdeabbf2181395b350a7cc5f9bfa744a87
Pranjal Soni <soni.pranjals17@gmail.com>
2023-09-03 23:58:00 +0530

    nvim: copy builtin editorconfigs' autocmd to trim trailing whitespaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -164,7 +164,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
   callback = function()
     local view = vim.fn.winsaveview()
-    vim.cmd [[%s/\s\+$//e]]
+    vim.cmd 'silent! undojoin'
+    vim.cmd [[silent keepjumps keeppatterns %s/\s\+$//e]]
     vim.fn.winrestview(view)
   end,
 })
