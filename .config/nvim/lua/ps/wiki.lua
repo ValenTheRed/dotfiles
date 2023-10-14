5ecb43446bf7350658bddacdd4d9d246eb53cbf4
Pranjal Soni <soni.pranjals17@gmail.com>
2023-10-14 16:32:42 +0530

    nvim: set a todo file autocmd

--- .config/nvim/lua/ps/wiki.lua
+++ .config/nvim/lua/ps/wiki.lua
@@ -1,7 +1,41 @@
-local wiki_index = "~/wiki/index.md"
+local dir = {
+  notes = vim.fn.expand([[~/notes]]),
+  todo = vim.fn.expand([[~/notes/todo.md]])
+}
 
-vim.keymap.set(
-  "n", "<leader>ww", ":e " .. wiki_index .. "<CR>", {
-    noremap = true, silent = true, desc = "open wiki's index file"
-  }
-)
+local function set_todos()
+  local fname = dir.todo
+  local today = vim.fn.strftime("%A, %d %b %Y")
+
+  local lines = vim.fn.readfile(fname)
+
+  -- NOTE: no error checking if top lines doesn't match the date.
+  if lines[1] == "# " .. today then
+    -- TODO: place cursor at lines#2 if it exists
+    return
+  end
+
+  lines[1] = "# " .. today
+
+  if #lines >= 2 then
+    local blankline = -1
+    for i, line in ipairs(lines) do
+      if string.match(line, "^$") then
+        blankline = i
+        break
+      end
+    end
+
+    table.remove(lines, blankline)
+    table.insert(lines, 2, "")
+  end
+
+  vim.fn.setline(1, lines)
+  -- vim.fn.writefile(lines, fname)
+end
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("parse_todo_file", { clear=true }),
+  pattern = dir.todo,
+  callback = set_todos,
+})
