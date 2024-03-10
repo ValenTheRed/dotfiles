1bb4329cf4a9e6da6e38e92825f7c3e2e6d7514e
Pranjal Soni <soni.pranjals17@gmail.com>
2024-03-10 22:32:58 +0530

    nvim: lsp: add lua lsp

--- .config/nvim/lua/ps/plugins/nvim_lspconfig.lua
+++ .config/nvim/lua/ps/plugins/nvim_lspconfig.lua
@@ -237,6 +237,37 @@ local config = function()
 			languages = efm_languages,
 		},
 	}
+	lsp.lua_ls.setup {
+		on_attach = on_attach,
+		capabilities = capabilities,
+		on_init = function(client)
+			local path = client.workspace_folders[1].name
+			if
+				vim.loop.fs_stat(path .. "/.luarc.json")
+				or vim.loop.fs_stat(path .. "/.luarc.jsonc")
+			then
+				return
+			end
+
+			client.config.settings.Lua =
+				vim.tbl_deep_extend("force", client.config.settings.Lua, {
+					runtime = {
+						version = "LuaJIT",
+					},
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+						},
+						-- Pull in all of 'runtimepath'. NOTE: this is slow
+						-- library = vim.api.nvim_get_runtime_file("", true)
+					},
+				})
+		end,
+		settings = {
+			Lua = {},
+		},
+	}
 end
 
 return { {
