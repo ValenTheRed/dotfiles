089d0f1d0f47dfc3226d3ba2ca8cd0de21c99b25
Pranjal Soni <soni.pranjals17@gmail.com>
2024-06-30 13:30:10 +0530

    ags: fix .editorconfig and reformat files

--- .config/ags/widgets/index.ts
+++ .config/ags/widgets/index.ts
@@ -1,3 +1,3 @@
-export { default as Date } from './date'
-export { default as Battery } from './battery'
-export { default as Workspace } from './workspace'
+export { default as Date } from "./date";
+export { default as Battery } from "./battery";
+export { default as Workspace } from "./workspace";
