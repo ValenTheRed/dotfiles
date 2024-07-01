293cfa43c4935d2356511701eb7a5e135700dac5
Pranjal Soni <soni.pranjals17@gmail.com>
2024-07-01 23:09:33 +0530

    ags: convert date status-widget to a Button

--- .config/ags/widgets/date.ts
+++ .config/ags/widgets/date.ts
@@ -2,7 +2,7 @@ const date = Variable("", {
     poll: [1000, "date '+%a, %e %b %l:%M %P'"],
 });
 
-export default Widget.Label({
+export default Widget.Button({
     class_name: "date status-widget",
     label: date.bind(),
 });
