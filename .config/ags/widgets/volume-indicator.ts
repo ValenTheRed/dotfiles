ca688c84927bfb4965ff240f3b253737f39aca74
Pranjal Soni <soni.pranjals17@gmail.com>
2024-07-02 19:55:28 +0530

    ags: fix: tooltip_text disappeared

--- .config/ags/widgets/volume-indicator.ts
+++ .config/ags/widgets/volume-indicator.ts
@@ -1,29 +1,32 @@
 /// Ref: https://github.com/Aylur/ags-docs/blob/e9722d5dd9c3720f5b38569a95ad0f5aa2619332/src/content/docs/services/audio.md
 import { VOLUME_INDICATOR_THRESHOLDS, WHITESUR_ICON_SIZE } from "constants";
-const audio = await Service.import("audio");
+const { speaker } = await Service.import("audio");
 
 export default Widget.Button({
     class_name: "volume-indicator status-widget",
-    on_clicked: () => (audio.speaker.is_muted = !audio.speaker.is_muted),
-    image: Widget.Icon({ size: WHITESUR_ICON_SIZE.VOLUME_INDICATOR }).hook(
-        audio.speaker,
-        (self) => {
+    on_clicked: () => (speaker.is_muted = !speaker.is_muted),
+    setup: (self) =>
+        self.hook(speaker, (self) => {
             let icon: string | undefined, tooltip_text: string;
+
             // The volume is not 0 when speaker is muted.
-            if (audio.speaker.is_muted) {
+            if (speaker.is_muted) {
                 tooltip_text = "Muted";
                 icon = "muted";
             } else {
-                const vol = audio.speaker.volume * 100;
+                const vol = speaker.volume * 100;
                 icon = VOLUME_INDICATOR_THRESHOLDS.find(
                     ([_, value]) => value <= vol,
                 )?.[0];
                 tooltip_text = `Volume ${Math.floor(vol)}%`;
             }
-            self.icon = icon
-                ? `audio-volume-${icon}-symbolic`
-                : "audio-speakers-symbolic";
+
+            self.image = Widget.Icon({
+                size: WHITESUR_ICON_SIZE.VOLUME_INDICATOR,
+                icon: icon
+                    ? `audio-volume-${icon}-symbolic`
+                    : "audio-card-symbolic",
+            });
             self.tooltip_text = tooltip_text;
-        },
-    ),
+        }),
 });
