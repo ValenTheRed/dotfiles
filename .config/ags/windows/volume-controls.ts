b9a3fa66ab7147169c597d0b414087f4c33aeb33
Pranjal Soni <soni.pranjals17@gmail.com>
2024-07-08 23:51:27 +0530

    ags: fix: listen to "notify::apps" signal to change list of app sliders

--- .config/ags/windows/volume-controls.ts
+++ .config/ags/windows/volume-controls.ts
@@ -3,7 +3,6 @@ import Gtk from "types/@girs/gtk-3.0/gtk-3.0";
 import { Stream } from "types/service/audio";
 
 const audio = await Service.import("audio");
-let previousApps = 0;
 
 const Slider = (speaker: Stream) =>
     Widget.Box({
@@ -67,20 +66,9 @@ export default (monitor: number = 0) =>
             vertical: true,
             children: [Slider(audio.speaker)],
             setup: (self) =>
-                // NOTE: `ags` v1.8.2 doesn't support `audio.apps` as a valid
-                // `Connectable`. Hence, a hook on `audio` and the global
-                // `previousApps`.
-                // TODO: make audio.apps a `Connectable`.
-                self.hook(audio, (self) => {
-                    if (
-                        audio.apps.length <= 0 ||
-                        audio.apps.length === previousApps
-                    ) {
-                        return;
-                    }
+                audio.connect("notify::apps", (audio) => {
                     const appSliders = audio.apps.map(Slider);
                     self.children = [Slider(audio.speaker), ...appSliders];
-                    previousApps = appSliders.length;
                 }),
         }),
     });
