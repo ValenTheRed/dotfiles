72878b36db40910d6420fb672466a2f43cf6260d
Pranjal Soni <soni.pranjals17@gmail.com>
2025-06-22 16:25:56 +0530

    fzf: source correct key-bindings

--- .config/fzf/fzf.zsh
+++ .config/fzf/fzf.zsh
@@ -1,3 +1,6 @@
-if [[ -f /etc/fedora-release ]]; then
+if [[ -f /usr/share/fzf/shell/key-bindings.zsh ]]; then
     source /usr/share/fzf/shell/key-bindings.zsh
+fi
+if [[ -f "/opt/homebrew/Cellar/fzf/0.42.0/shell/key-bindings.zsh" ]]; then
+    source "/opt/homebrew/Cellar/fzf/0.42.0/shell/key-bindings.zsh"
 fi
