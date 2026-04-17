4aa0ba8410fc36f0fae9d9f219ed78ca3d4940d4
Pranjal Soni <soni.pranjals17@gmail.com>
2026-04-17 18:24:11 +0530

    nvim: fix: deprecated APIs for diagnostic

--- .config/nvim/lua/ps/vim_diagnostic.lua
+++ .config/nvim/lua/ps/vim_diagnostic.lua
@@ -6,41 +6,59 @@ local nmap = function(lhs, rhs, desc)
 	})
 end
 
--- Switch off diagnostics signs in the sign column.
--- github issue: https://github.com/neovim/neovim/issues/15770
--- :help diagnostic-handlers-example
--- vim.diagnostic.handlers.signs = {
---   show = false,
--- }
-local M = {}
-
-vim.diagnostic.config {
-	virtual_text = {
-		prefix = "●",
-	},
-	signs = false,
-	float = {
-		source = "always",
-	},
+local texthl = {
+	[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+	[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+	[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+	[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+}
+local text = {
+	[vim.diagnostic.severity.ERROR] = " ",
+	[vim.diagnostic.severity.WARN] = " ",
+	[vim.diagnostic.severity.INFO] = " ",
+	[vim.diagnostic.severity.HINT] = "󰌵 ",
 }
 
--- Diagnostic signs
-M.icons = { error = " ", warn = " ", info = " ", hint = "󰌵 " }
-
--- set icon and text highlight for diagnostic signs
-for type, icon in pairs(M.icons) do
-	local title_case = string.upper(string.sub(type, 1, 1))
-		.. string.sub(type, 2)
-	local hl = "DiagnosticSign" .. title_case
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = nil })
-end
+vim.diagnostic.config {
+	virtual_text = { prefix = "●", },
+	signs = false,
+	status = {
+		format = function(counts)
+			local items = {}
+			for level, _ in ipairs(vim.diagnostic.severity) do
+				local count = counts[level] or 0
+				if count > 0 then
+					table.insert(
+						items,
+						("%%#%s#%s%s"):format(texthl[level], text[level], count)
+					)
+				end
+			end
+			return table.concat(items, " ")
+		end
+	}
+}
 
 -- d- is the same as d1k. So useless and available to map.
-nmap("d-", function()
-	vim.diagnostic.open_float { scope = "line", source = true }
-end, "open line diagnostics in a floating window")
+nmap(
+	"d-",
+	function()
+		vim.diagnostic.open_float { scope = "line", source = true }
+	end,
+	"open line diagnostics in a floating window"
+)
 nmap("dl", vim.diagnostic.setloclist, "vim.diagnostic.setloclist")
-nmap("[d", vim.diagnostic.goto_prev, "jump to previous diagnostic in buffer")
-nmap("]d", vim.diagnostic.goto_next, "jump to next diagnostic in buffer")
-
-return M
+nmap(
+	"[d",
+	function()
+		vim.diagnostic.jump({ count = -1, float = true })
+	end,
+	"jump to previous diagnostic in buffer"
+)
+nmap(
+	"]d",
+	function()
+		vim.diagnostic.jump({ count = 1, float = true })
+	end,
+	"jump to next diagnostic in buffer"
+)
